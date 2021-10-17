55fd090b48d61985de3a8448a1d5a51541f78e0f
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2021-10-17 14:16:05 +0100

    tone.js installed

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
-console.log('Hello world!')
+import * as Tone from 'tone'
+
+//clear the console
+console.clear()
+
+//run button
+const run = document.querySelector('#run')
+
+const sampler = new Tone.Sampler({
+  urls: {
+    A1: 'A1.mp3',
+    A2: 'A2.mp3'
+  },
+  baseUrl: 'https://tonejs.github.io/audio/casio/',
+  onload: () => {
+    sampler.triggerAttackRelease(['C1', 'E1', 'G1', 'B1'], 0.5)
+  }
+}).toDestination()
+
+run.addEventListener('click', () => Tone.start())
